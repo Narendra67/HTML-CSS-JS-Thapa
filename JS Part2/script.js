bf83ff30bd3a71b1cd33b2894adcd1249887181c
Narendra Kushwaha <Narendra Kushwaha>
2024-10-09 12:08:29 +0530

    Async await method

--- JS Part2/script.js
+++ JS Part2/script.js
@@ -1,20 +1,42 @@
 //promises
 
-let promps1 = new Promise((resolve, reject) =>{
-    let a = Math.random()
-        if (a < 0.5) {
-            reject("No random number was not supporting you")
+// let promps1 = new Promise((resolve, reject) =>{
+//     let a = Math.random()
+//         if (a < 0.5) {
+//             reject("No random number was not supporting you")
             
-        }
-        else{
+//         }
+//         else{
+//             setTimeout(() => {
+//                 console.log('Yes, this is done')
+//                 resolve("Narendra")
+//             }, 2000);
+//         }
+//         })
+
+
+// promps1.then((a) =>{
+//     console.log(a)
+// }).catch((err)=>{
+//     console.log(err)
+// });
+
+
+// Async await
+
+ async function getData(){
+        return new Promise((resolve, reject) =>{
             setTimeout(() => {
-                console.log('Yes, this is done')
-                resolve("Narendra")
-            }, 2000);
-        }
+                resolve(4300)
+            }, 2500);
         })
+}
+async function main() {
+    console.log("Loading modules");
+    console.log("Do something else")
+    let data = await getData()
+        console.log(data)
+        console.log('Yes this is await function')
+    }
 
-
-promps1.then((a) =>{
-    console.log(a)
-})
+    main();
