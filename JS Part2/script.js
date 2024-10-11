cad5f8642bc9c9c60ab0c7ef3aa857cb5470e8e2
Narendra Kushwaha <Narendra Kushwaha>
2024-10-11 10:31:04 +0530

    JS topics

--- JS Part2/script.js
+++ JS Part2/script.js
@@ -24,19 +24,97 @@
 
 // Async await
 
- async function getData(){
-        return new Promise((resolve, reject) =>{
-            setTimeout(() => {
-                resolve(4300)
-            }, 2500);
-        })
-}
-async function main() {
-    console.log("Loading modules");
-    console.log("Do something else")
-    let data = await getData()
-        console.log(data)
-        console.log('Yes this is await function')
-    }
 
-    main();
+//  async function getData(){
+//         return new Promise((resolve, reject) =>{
+//             setTimeout(() => {
+//                 resolve(4300)
+//             }, 2500);
+//         })
+// }
+// async function main() {
+//     console.log("Loading modules");
+//     console.log("Do something else")
+//     let data = await getData()
+//         console.log(data)
+//         console.log('Yes this is await function')
+//     }
+
+//     main();
+
+// async function getData(){
+//     // Simulate getting data from a Server
+//     let x = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//     let data = await x.json();
+//     return data;
+
+//     }
+//     async function main() {
+//         console.log("Loading modules");
+//         console.log("Do something else")
+//         let data = await getData()
+//             console.log(data)
+//             console.log('Yes this is await function')
+//         }
+    
+//         main();
+
+
+
+// let count = true;
+// const p1 = new Promise(function(resolve, reject){
+//     if(count){
+//         resolve("Async task is Completed")
+//     }
+//     else{
+//         reject("Error")
+//     }
+// })
+// console.log(p1);
+
+
+// Object oriented Programming(in JS)
+
+// const obj = {
+//     name: "Narendra",
+//     age: 22
+// }
+
+// console.log(obj)
+// console.log(obj.name);
+
+
+// class Animal{
+//     constructor(name){
+//         this.name = name;
+//         console.log("Object is created");
+//     }
+
+//     eats(){
+//         console.log("I'm eating")
+//     }
+//     jumps(){
+//         console.log("they jumped")
+//     }
+// }
+// class Lion extends Animal{
+//     constructor(name){
+//         super(name);
+//         console.log("Object is created and he is a lion")
+//     }
+
+// }
+// let a = new Animal("kalii");
+// console.log(a);
+// let lion = new Lion("Sherru");
+// console.log(lion);
+
+
+
+// document.getElementById("myBtn").onclick = function(){
+//     alert("Welcome again");
+// }
+// document.getElementById('myBtn2').addEventListener('click', function(){
+//     alert("Welcome to button 2")
+// })
+
